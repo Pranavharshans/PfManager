45aad33dfd2992f1466dac9587e43f5161fc6af9
Pranavharshans <pranavharshan.s2021cseb@sece.ac.in>
2024-11-19 19:03:08 +0530

    update

--- src/components/StatementManager.tsx
+++ src/components/StatementManager.tsx
@@ -534,3 +534,14 @@ function getTimeOfDay() {
   if (hour < 18) return 'afternoon'
   return 'evening'
 }
+
+
+console.log({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+});
